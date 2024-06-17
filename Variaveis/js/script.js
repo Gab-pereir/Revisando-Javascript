bfb511bccf701509c5d24d9cd9ee5027fc83307d
Gabriel Pereira de Almeida <pessoal.gabriel.pereira@gmail.com>
2024-06-17 11:15:18 -0300

    add folder "variaveis"

---
+++ Variaveis/js/script.js
@@ -0,0 +1,29 @@
+var marcaCarro = 'Fiat';
+let tipoTranporte = 'Onibus';
+const vendido = false;
+
+
+console.log(marcaCarro);
+
+// é possível alterar o valor
+
+var nome = 'Gabriel';
+nome = 'Leticia';
+console.log(nome);
+
+// é possível alterar o valor
+
+let sobrenome = 'pereira';
+sobrenome = 'almeida';
+console.log(sobrenome);
+
+// Não é possível alterar o valor
+const marca = 'Meraki';
+console.log(marca);
+
+let valor01 = 10;
+let valor02 = 50;
+
+resultado = valor01 + valor02;
+
+console.log(resultado);
