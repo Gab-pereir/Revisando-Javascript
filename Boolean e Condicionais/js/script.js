58a30dd890bcf7fef7225712f5d0e4fa4a3ce34b
Gabriel Pereira de Almeida <pessoal.gabriel.pereira@gmail.com>
2024-06-17 16:31:12 -0300

    add folder "Boolean e Condicionais"

---
+++ Boolean e Condicionais/js/script.js
@@ -0,0 +1,27 @@
+let carroVendido = true;
+
+if(carroVendido){
+    console.log('O carro foi vendido');
+} else {
+    console.log('O carro não foi vendido');
+}
+
+let possuiMoto = true;
+let PossuiCarro = false;
+
+if(possuiMoto) {
+    console.log('Ele tem uma moto');
+} else if(PossuiCarro) {
+    console.log('Ele possui carro');
+} else{
+    console.log('Ele não tem veiculo')
+}
+
+//Valores falsos
+
+//if(false)
+//if(0)
+//if(Nan)
+//if(null)
+//if(undefined)
+//if('')
