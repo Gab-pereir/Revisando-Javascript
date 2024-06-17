1fdc88e5d838bd8dc5889f42ace793c0b1a200c5
Gabriel Pereira de Almeida <pessoal.gabriel.pereira@gmail.com>
2024-06-17 16:53:26 -0300

    add folder "Objetos"

---
+++ Objetos/js/script.js
@@ -0,0 +1,15 @@
+var carro = {
+    nome: 'Celta',
+    ano: 2010,
+    cor: 'Preto',
+    automatico: false
+};
+
+console.log(carro);
+console.log(carro.nome);
+console.log(carro.ano);
+console.log(carro.cor);
+console.log(carro.automatico);
+
+carro.nome = 'Corsa';
+console.log(carro.nome);
