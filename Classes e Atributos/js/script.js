6378ae2d59ea1762e472ef54fd6fe600ab72041a
Gabriel Pereira de Almeida <pessoal.gabriel.pereira@gmail.com>
2024-07-02 11:33:29 -0300

    add folder "Classes e Atributos"

---
+++ Classes e Atributos/js/script.js
@@ -0,0 +1,9 @@
+//classList: Retorna uma lista com as classes que existem no elemento. é possível adicionar, remover e verificar se existe uma classe no elemento
+
+//const button =document.getElementById('js-btn');
+
+//button.addEventListener('click', () => {
+//    button.classList.toggle('active');
+//})
+
+//getAttribute & setAttribute: Métodos que retornam ou definem de acordo com o atributo selecionado
